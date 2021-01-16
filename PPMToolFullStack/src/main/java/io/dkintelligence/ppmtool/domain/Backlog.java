36173579cfa8dc7ce2c62b9e54513f8a77b2c8d3
DraganKal <dragan.kalakovic@yahoo.com>
2021-01-16 12:39:54 +0100

    Backlog - ProjectTask relationship

--- PPMToolFullStack/src/main/java/io/dkintelligence/ppmtool/domain/Backlog.java
+++ PPMToolFullStack/src/main/java/io/dkintelligence/ppmtool/domain/Backlog.java
@@ -4,6 +4,8 @@ package io.dkintelligence.ppmtool.domain;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Backlog {
@@ -17,7 +19,8 @@ public class Backlog {
     @JoinColumn(name = "project_id", nullable = false)
     @JsonIgnore
     private Project project;
-//    OneToMany projectTasks
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "backlog")
+    private List<ProjectTask> projectTasks = new ArrayList<>();
 
     public Backlog() {
     }
@@ -45,6 +48,12 @@ public class Backlog {
     }
     public void setProject(Project project) {
         this.project = project;
+    }
+    public List<ProjectTask> getProjectTasks() {
+        return projectTasks;
+    }
+    public void setProjectTasks(List<ProjectTask> projectTasks) {
+        this.projectTasks = projectTasks;
     }
 
 }
--- PPMToolFullStack/src/main/java/io/dkintelligence/ppmtool/domain/ProjectTask.java
+++ PPMToolFullStack/src/main/java/io/dkintelligence/ppmtool/domain/ProjectTask.java
@@ -1,4 +1,6 @@
 package io.dkintelligence.ppmtool.domain;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.util.Date;
@@ -17,7 +19,11 @@ public class ProjectTask {
     private String status;
     private Integer priority;
     private Date dueDate;
-//    ManyToOne with Backlog
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.REFRESH)
+    @JoinColumn(name = "backlog_id", updatable = false, nullable = false)
+    @JsonIgnore
+    private Backlog backlog;
+
     @Column(updatable = false)
     private String projectIdentifier;
 
@@ -30,81 +36,68 @@ public class ProjectTask {
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getProjectSequence() {
         return projectSequence;
     }
-
     public void setProjectSequence(String projectSequence) {
         this.projectSequence = projectSequence;
     }
-
     public String getSummary() {
         return summary;
     }
-
     public void setSummary(String summary) {
         this.summary = summary;
     }
-
     public String getAcceptanceCriteria() {
         return acceptanceCriteria;
     }
-
     public void setAcceptanceCriteria(String acceptanceCriteria) {
         this.acceptanceCriteria = acceptanceCriteria;
     }
-
     public String getStatus() {
         return status;
     }
-
     public void setStatus(String status) {
         this.status = status;
     }
-
     public Integer getPriority() {
         return priority;
     }
-
     public void setPriority(Integer priority) {
         this.priority = priority;
     }
-
     public Date getDueDate() {
         return dueDate;
     }
-
     public void setDueDate(Date dueDate) {
         this.dueDate = dueDate;
     }
-
     public String getProjectIdentifier() {
         return projectIdentifier;
     }
-
     public void setProjectIdentifier(String projectIdentifier) {
         this.projectIdentifier = projectIdentifier;
     }
-
     public Date getCreated_At() {
         return created_At;
     }
-
     public void setCreated_At(Date created_At) {
         this.created_At = created_At;
     }
-
     public Date getUpdate_At() {
         return update_At;
     }
-
     public void setUpdate_At(Date update_At) {
         this.update_At = update_At;
+    }
+    public Backlog getBacklog() {
+        return backlog; }
+
+    public void setBacklog(Backlog backlog) {
+        this.backlog = backlog;
     }
 
     @PrePersist
