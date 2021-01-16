04dde85ae13a9c46472865b8b888de475992e084
DraganKal <dragan.kalakovic@yahoo.com>
2021-01-16 13:21:46 +0100

    Persist Project Task

---
+++ PPMToolFullStack/src/main/java/io/dkintelligence/ppmtool/services/ProjectTaskService.java
@@ -0,0 +1,37 @@
+package io.dkintelligence.ppmtool.services;
+
+import io.dkintelligence.ppmtool.domain.Backlog;
+import io.dkintelligence.ppmtool.domain.ProjectTask;
+import io.dkintelligence.ppmtool.repositories.BacklogRepository;
+import io.dkintelligence.ppmtool.repositories.ProjectTaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectTaskService {
+
+    @Autowired
+    private BacklogRepository backlogRepository;
+    @Autowired
+    private ProjectTaskRepository projectTaskRepository;
+
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask){
+
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+        projectTask.setBacklog(backlog);
+        Integer backlogSequence = backlog.getPTSequence();
+        backlogSequence++;
+        projectTask.setProjectSequence(projectIdentifier + "-" + backlogSequence);
+        projectTask.setProjectIdentifier(projectIdentifier);
+//        if(projectTask.getPriority() == 0 || projectTask.getPriority() == null){
+//            projectTask.setPriority(3);
+//        }
+        if(projectTask.getStatus() == "" || projectTask.getStatus() == null){
+            projectTask.setStatus("TO_DO");
+        }
+
+        return projectTaskRepository.save(projectTask);
+
+    }
+
+}
