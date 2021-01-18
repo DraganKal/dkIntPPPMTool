c68d2708090f6ee10be495b022dab5bbb5617f21
DraganKal <dragan.kalakovic@yahoo.com>
2021-01-18 22:58:54 +0100

    Organize Project Tasks by status and priority

--- ppmtool-react-client/src/components/ProjectBoard/Backlog.js
+++ ppmtool-react-client/src/components/ProjectBoard/Backlog.js
@@ -9,6 +9,20 @@ class Backlog extends Component {
       <ProjectTask key={project_task.id} project_task={project_task} />
     ));
 
+    let todoItems = [];
+    let inProgressItems = [];
+    let doneItems = [];
+
+    for (let i = 0; i < tasks.length; i++) {
+      if (tasks[i].props.project_task.status == "TO_DO") {
+        todoItems.push(tasks[i]);
+      } else if (tasks[i].props.project_task.status == "IN_PROGRESS") {
+        inProgressItems.push(tasks[i]);
+      } else {
+        doneItems.push(tasks[i]);
+      }
+    }
+
     return (
       <div className="container">
         <div className="row">
@@ -18,8 +32,7 @@ class Backlog extends Component {
                 <h3>TO DO</h3>
               </div>
             </div>
-
-            {tasks}
+            {todoItems}
           </div>
           <div className="col-md-4">
             <div className="card text-center mb-2">
@@ -27,10 +40,7 @@ class Backlog extends Component {
                 <h3>In Progress</h3>
               </div>
             </div>
-            {
-              //    <!-- SAMPLE PROJECT TASK STARTS HERE -->
-              // <!-- SAMPLE PROJECT TASK ENDS HERE -->\
-            }
+            {inProgressItems}
           </div>
           <div className="col-md-4">
             <div className="card text-center mb-2">
@@ -38,10 +48,7 @@ class Backlog extends Component {
                 <h3>Done</h3>
               </div>
             </div>
-            {
-              //  <!-- SAMPLE PROJECT TASK STARTS HERE -->
-              // <!-- SAMPLE PROJECT TASK ENDS HERE -->
-            }
+            {doneItems}
           </div>
         </div>
       </div>
