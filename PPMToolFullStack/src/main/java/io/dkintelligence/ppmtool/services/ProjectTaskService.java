2a8c2e1367635d7e718844932857862d2689a74a
DraganKal <dragan.kalakovic@yahoo.com>
2021-01-18 10:49:10 +0100

    Bug fix

--- PPMToolFullStack/src/main/java/io/dkintelligence/ppmtool/services/ProjectTaskService.java
+++ PPMToolFullStack/src/main/java/io/dkintelligence/ppmtool/services/ProjectTaskService.java
@@ -83,10 +83,6 @@ public class ProjectTaskService {
     public void deleteProjectTaskByProjectSequence(String backlog_id, String pt_id){
 
         ProjectTask projectTask = findProjectTaskByProjectSequence(backlog_id, pt_id);
-        Backlog backlog = projectTask.getBacklog();
-        List<ProjectTask> pts = backlog.getProjectTasks();
-        pts.remove(projectTask);
-        backlogRepository.save(backlog);
         projectTaskRepository.delete(projectTask);
 
     }
