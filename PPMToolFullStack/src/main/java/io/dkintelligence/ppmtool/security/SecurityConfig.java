d440feef53c9877e54f38f219d87b741c657a088
DraganKal <dragan.kalakovic@yahoo.com>
2021-01-28 12:24:16 +0100

    Initial Security Config

--- PPMToolFullStack/src/main/java/io/dkintelligence/ppmtool/security/SecurityConfig.java
+++ PPMToolFullStack/src/main/java/io/dkintelligence/ppmtool/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package io.dkintelligence.ppmtool.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,10 +17,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 )
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private JwtAuthenticationEntryPoint unauthorizedHandler;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
-                .exceptionHandling().authenticationEntryPoint().and()
+                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
