09aac701aa1f957fa6e2e90e7f1cca6a95c47c7e
DraganKal <dragan.kalakovic@yahoo.com>
2021-01-28 13:57:47 +0100

    User registration part 1

--- PPMToolFullStack/src/main/java/io/dkintelligence/ppmtool/web/UserController.java
+++ PPMToolFullStack/src/main/java/io/dkintelligence/ppmtool/web/UserController.java
@@ -1,7 +1,38 @@
 package io.dkintelligence.ppmtool.web;
 
+import io.dkintelligence.ppmtool.domain.User;
+import io.dkintelligence.ppmtool.services.MapValidationErrorService;
+import io.dkintelligence.ppmtool.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+    @Autowired
+    private UserService userService;
 
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result){
+//        Validate passwords match
+
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if(errorMap != null)return errorMap;
+
+        User newUser = userService.saveUser(user);
+
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
 
 }
